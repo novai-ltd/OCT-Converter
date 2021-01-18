59d1b2e0150ff221fd917f373169f281d99e5f33
jmaddison <john.maddison@gmail.com>
2021-01-18 11:24:25 +0000

    added test convertion

---
+++ examples/demo_p2_amd.py
@@ -0,0 +1,34 @@
+from oct_converter.readers import E2E
+import cv2
+import os
+
+
+indir = r'E:\data\e2e\1'
+#filepath = os.path.join(indir,'OCT exsample 1.E2E')
+filenames = ['02401M.e2e', '02402M.e2e', '02403M.e2e', '02404M.e2e', '02405M.e2e', '02406M.e2e', '02407M.e2e']#,SBL5004A '026_F016.E2E', '02601C.E2E']
+for filename in filenames:
+    filepath = os.path.join(indir,filename)
+    #
+    # filepath = '/Users/mark/Downloads/TRALO01R.E2E'
+    file = E2E(filepath)
+    cslo_imgs = file.read_clso_image()  # returns an cslo volume with additional metadata if available
+
+    # cv2.imshow('img1',cslo_imgs[0])
+    # cv2.imshow('img2',cslo_imgs[1])
+    # cv2.waitKey()
+    #oct_volumes = file.read_oct_volume()  # returns an OCT volume with additional metadata if available
+    #
+    # for vi in range(len(oct_volumes) ):
+    #     volume = oct_volumes[vi]
+    #     os.makedirs(os.path.join(indir, 'out', filename, 'oct', str(vi)), exist_ok=True)
+    #     #volume.peek()  # plots a montage of the volume
+    #
+    #     for i in range(volume.num_slices):
+    #         cv2.imwrite(os.path.join(indir, 'out', filename, 'oct', str(vi), '{0}.png'.format(i)), volume.volume[i])
+    #
+    #         #volume.save(os.path.join(indir,'out',filename,'oct','{}.avi'.format(volume.patient_id)))
+
+
+    for idx in range(len(cslo_imgs)):
+        os.makedirs(os.path.join(indir,'out',filename,'cslo'), exist_ok=True)
+        cv2.imwrite(os.path.join(indir,'out',filename,'cslo','{0}.png'.format(idx)),cslo_imgs[idx])
